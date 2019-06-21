77671a48a26f3edf764121b5d069568320af7a56
ndwarshuis <natedwarshuis@gmail.com>
2019-06-20 23:07:28 -0400

    ADD test function to create fake org elements

--- test/test-helper.el
+++ test/test-helper.el
@@ -13,3 +13,20 @@
   "Run BODY in a sandboxed Org directory."
   `(let ((org-directory (f-join test-root "Org")))
      ,@body))
+
+(defun string-to-org-element (s)
+  "Convert S to org element. This assume S is valid org syntax."
+  (cl-labels
+      ((get-contents
+        (elem)
+        (let ((type (org-element-type elem)))
+          (if (or (eq type 'paragraph) (eq type 'section))
+              (get-contents (car (org-element-contents elem)))
+            elem))))
+    (with-temp-buffer
+      (insert s)
+      (->> (org-element-parse-buffer)
+           (org-element-contents)
+           (car)
+           (get-contents)))))
+         
