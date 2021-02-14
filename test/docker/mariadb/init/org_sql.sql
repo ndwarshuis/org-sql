a28d7f3f8df3e401caf75490e2d0782705a3faae
ndwarshuis <ndwar@yavin4.ch>
2021-02-13 22:19:59 -0500

    FIX wonky mysql syntax bug stuff

--- test/docker/mariadb/init/org_sql.sql
+++ test/docker/mariadb/init/org_sql.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE org_sql;
 USE org_sql;
 
-CREATE USER org_sql IDENTIFIED BY 'org_sql'@'locahost';
-GRANT CREATE, DROP, DELETE, INSERT, SELECT ON 'org_sql' TO 'org_sql'@'localhost';
+CREATE USER 'org_sql' IDENTIFIED BY 'org_sql';
+-- MySQL needs to REFERENCES permission for foreign keys, MariaDB apparently
+-- ignores it
+GRANT CREATE, DROP, DELETE, INSERT, REFERENCES, SELECT ON org_sql.* TO 'org_sql';
