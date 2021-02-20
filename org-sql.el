19eaba4dcadb3f1101a2363aac2a6d2430559c70
ndwarshuis <ndwar@yavin4.ch>
2021-02-20 16:46:01 -0500

    ENH bump version

--- org-sql.el
+++ org-sql.el
@@ -6,7 +6,7 @@
 ;; Keywords: org-mode, data
 ;; Homepage: https://github.com/ndwarshuis/org-sql
 ;; Package-Requires: ((emacs "27.1") (s "1.12") (f "0.20.0") (dash "2.17") (org-ml "5.6.0"))
-;; Version: 2.0.0
+;; Version: 2.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
